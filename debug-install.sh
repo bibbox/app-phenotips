cb0fe1c51cf57f3ac2c382d83d1e01a793bce25e
wp5 <bbmri.wp5@wp5>
2016-11-07 16:04:24 +0100

    updateOnDevServer

--- debug-install.sh
+++ debug-install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-./install.sh   --folder /Users/mue/dockerdata/phenotips-phenotips \
+rm -rf /home/wp5/dev/dockerdata/phenotips-phenotips
+./install.sh   --folder  /home/wp5/dev/dockerdata/phenotips-phenotips \
                --instance phenotips  \
                --port     8089  \
                --MYSQL_ROOT_PASSWORD bibboxadmin \
                --MYSQL_DATABASE phenotips \
                --MYSQL_USER  phenotips  \
+               --EXTAPI_PORT 9231  \
                --MYSQL_PASSWORD      bibboxadmin
-               
-               
+
+more /home/wp5/dev/dockerdata/phenotips-phenotips/docker-compose.yml
+echo
+echo ls /home/wp5/dev/dockerdata/phenotips-phenotips/
+echo docker-compose -f /home/wp5/dev/dockerdata/phenotips-phenotips/docker-compose.yml up -d 
+
