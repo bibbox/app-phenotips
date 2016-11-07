843dce1b9b653be60898784b66d903d80ff774a2
lomadi <lomadi@mac.com>
2016-11-07 17:37:04 +0100

    add c&p

---
+++ cutandpaste.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/bibbox/app-phenotips.git 
+
+git pull
+
+git add .
+git commit -m updateOnDevServer 
+git push -u origin master
+
+git remote add origin https://github.com/bibbox/app-phenotips.git 
+
