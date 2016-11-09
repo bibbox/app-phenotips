13c16ecef58eaeb3c53bb076a7178aafb3e152c1
reihsr <robert.reihs@gmail.com>
2016-11-09 10:31:13 +0100

    update wrong script folder

--- images/phenotips/scripts/entrypoint.sh
+++ images/phenotips/scripts/entrypoint.sh
@@ -2,7 +2,7 @@
 echo "Starting Phenotuos Application Container!"
 
 echo "Wait for DB server to be ready"
-/opt/scripts/waitforit.sh "phenotips-db:3306"
+/opt/bibbox-scripts/waitforit.sh "phenotips-db:3306"
 
 /usr/local/tomcat/bin/startup.sh
 tail -f /usr/local/tomcat/logs/catalina.out
