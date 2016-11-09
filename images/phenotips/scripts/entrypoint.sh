ab883e10012faa6ca096aa60a55dd70a5ad5d6dd
reihsr <robert.reihs@gmail.com>
2016-11-09 10:19:09 +0100

    Update Installation

--- images/phenotips/scripts/entrypoint.sh
+++ images/phenotips/scripts/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Starting Phenotuos Application Container!"
 
+echo "Wait for DB server to be ready"
+/opt/scripts/waitforit.sh "phenotips-db:3306"
+
 /usr/local/tomcat/bin/startup.sh
 tail -f /usr/local/tomcat/logs/catalina.out
