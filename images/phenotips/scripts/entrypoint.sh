fa17023bc68b021d0492e2a4781e35132636c39c
Emilian Jungwirth <emilian.jung+github@gmail.com>
2022-01-31 13:44:35 +0100

    Updated file structure for BIBBOXv4

--- images/phenotips/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting PhenoTips Application Container!"
-
-echo "Wait for DB server to be ready"
-/opt/bibbox-scripts/waitforit.sh "phenotips-db:3306"
-
-/usr/local/tomcat/bin/startup.sh
-tail -f /usr/local/tomcat/logs/catalina.out
